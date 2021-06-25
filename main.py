2faa27940170a247fe2b509c98d4e4b277f39c93
alangshur <adlangshur@gmail.com>
2021-06-24 20:42:24 -0500

    Completed base features for SEC 13F web scraper

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from src.utils.mapping import MappingModule
 mapping_module = MappingModule()
 scraper = SEC13FWebScraper(mapping_module)
 
-since = datetime(2021, 4, 18)
-filing_ids, next_query_dt = scraper.fetch_filing_ids(since)
+# since = datetime(2021, 4, 18)
+# filing_ids, next_query_dt = scraper.fetch_filing_ids(since)
 
-print('Num filing ids: ' + str(len(filing_ids)))
-print('Next query dt: ' + next_query_dt)
-for id in filing_ids:
-    print(id, flush=True)
+# print('Num filing ids: ' + str(len(filing_ids)))
+# print('Next query dt: ' + next_query_dt)
+# for id in filing_ids:
+#     print(id, flush=True)
+
+filing_data, holdings_data = scraper.fetch_filing_data('0001134008-21-000002')
+print(filing_data)
+print('\n\n\n\n')
+print(holdings_data)
--- src/web/sec.py
+++ src/web/sec.py
@@ -1,14 +1,14 @@
 from typing import Tuple, Optional, Union
 from requests_html import HTMLSession
 import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from datetime import datetime
 import re
 
-from src.utils.logger import BaseModule
+from src.utils.logger import BaseModuleWithLogging
 
 
-class SEC13FWebScraper(BaseModule):
+class SEC13FWebScraper(BaseModuleWithLogging):
 
     def __init__(self, mapping_module,
                  filing_links_url: str='https://sec.report/loadmore.php',
@@ -19,15 +19,15 @@ class SEC13FWebScraper(BaseModule):
         self.filing_links_url = filing_links_url
         self.filing_url = filing_url
 
-    def fetch_filing_ids(self, since: Union[datetime, str], 
+    def fetch_filing_ids(self, since_dt: Union[datetime, str], 
                          form_number: str='13F-HR') -> Optional[Tuple[list, str]]:
 
         try:
 
             # get query dt
-            if isinstance(since, datetime): dt = since.strftime('%Y-%m-%d %H:%M:%S')
-            elif isinstance(since, str): dt = since
-            else: raise Exception('invalid type for \'since\'')
+            if isinstance(since_dt, datetime): dt = since_dt.strftime('%Y-%m-%d %H:%M:%S')
+            elif isinstance(since_dt, str): dt = since_dt
+            else: raise Exception('invalid type for \'since_dt\'')
 
             # load and render webpage
             session = HTMLSession()
@@ -55,7 +55,7 @@ class SEC13FWebScraper(BaseModule):
             return filing_ids, next_query_dt
 
         except Exception as e:
-            self.logger.error('Exception in fetch_filing_ids: {}.'.format(e))
+            self.logger.exception('Exception in fetch_filing_ids: {}.'.format(e))
             return None
 
     def fetch_filing_data(self, filing_id: str) -> Optional[Tuple[dict, dict]]:
@@ -64,6 +64,7 @@ class SEC13FWebScraper(BaseModule):
             # load and render webpage
             session = HTMLSession()
             r = session.get(self.filing_url + filing_id + '/' + filing_id + '.txt')
+            r.raise_for_status()
             r.html.render()
             session.close()
 
@@ -73,33 +74,67 @@ class SEC13FWebScraper(BaseModule):
 
             # extract filing ids
             xml_filing_data = content_html.findAll('xml')
-            if len(xml_filing_data) != 2: raise Exception('unexpected number of xml documents')
-    
-            # TODO: parse both xml tables
+            if len(xml_filing_data) != 2: 
+                raise Exception('unexpected number of xml documents')
+
+            # extract filing/holdings data
+            filing_data, holdings_data = xml_filing_data
+            filing_data = self.extract_filing_data(filing_data)
+            holdings_data = self.extract_filing_holdings_data(holdings_data)
+
+            return filing_data, holdings_data
 
         except Exception as e:
-            self.logger.error('Exception in fetch_filing_data_links: {}.'.format(e))
+            self.logger.exception('Exception in fetch_filing_data_links: {}.'.format(e))
             return None
 
-    def fetch_filing_holdings(self, holdings_link: str) -> Optional[list]:
+    def extract_filing_data(self, filing_data: Tag) -> dict:
 
-        # load and render webpage
-        session = HTMLSession()
-        r = session.get(holdings_link)
-        r.html.render()
-        session.close()
+        # extract report data
+        submission_type = filing_data.find('submissiontype').get_text()
+        report_period = filing_data.find('periodofreport').get_text()
+        form_file_no = filing_data.find('form13ffilenumber').get_text()
 
-        # parse xml        
-        tree = ET.parse(r.content.decode('utf-8'))
-        root = tree.getroot()
+        # extract company data
+        company_name = filing_data.find('filingmanager').find('name').get_text()
+        cik = filing_data.find('cik').get_text()
 
-        # load holdings data
-        holdings_data = []
-        for holding in root:
-            holding_data = {
-                'issuer_name': holding[0],
-                'class_title': holding[1],
-                'cusip': holding[2],
-                'value': float(holding[3]) * 100,
-                'shares': int(holding[4][0])
-            }
+        # extract holdings summary data
+        total_holdings_entries = int(filing_data.find('tableentrytotal').get_text())
+        total_holdings_value = int(filing_data.find('tablevaluetotal').get_text()) * 1000
+        confidential_omitted = bool(filing_data.find('isconfidentialomitted').get_text() == 'true')
+
+        return {
+            'submission_type': submission_type,
+            'report_period': report_period,
+            'form_file_no': form_file_no,
+            'company_name': company_name,
+            'cik': cik,
+            'total_holdings_entries': total_holdings_entries,
+            'total_holdings_value': total_holdings_value,
+            'confidential_omitted': confidential_omitted
+        }
+
+    def extract_filing_holdings_data(self, holdings_data: Tag) -> dict:
+
+        # extract holdings data
+        issuer_name_s = holdings_data.findAll('nameofissuer')
+        class_title_s = holdings_data.findAll('titleofclass')
+        cusip_s = holdings_data.findAll('cusip')
+        value_s = holdings_data.findAll('value')
+        shares_s = holdings_data.findAll('sshprnamt')
+        investment_discretion_s = holdings_data.findAll('investmentdiscretion')
+
+        # filter tag data
+        filt_holdings = [] 
+        for i in range(len(issuer_name_s)):
+            filt_holdings.append({
+                'issuer_name': issuer_name_s[i].get_text(),
+                'class_title': class_title_s[i].get_text(),
+                'cusip': cusip_s[i].get_text(),
+                'value': int(value_s[i].get_text()) * 1000,
+                'shares': int(shares_s[i].get_text()),
+                'investment_discretion': investment_discretion_s[i].get_text()
+            })
+
+        return filt_holdings
