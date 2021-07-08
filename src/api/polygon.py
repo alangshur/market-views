3cc9ee4c88bb3b164dfe332becc14839d53db7c6
alangshur <adlangshur@gmail.com>
2021-07-07 21:29:41 -0500

    Added attempt settings to polygon api

--- src/api/polygon.py
+++ src/api/polygon.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 from src.api.base import APIBaseConnector
 
@@ -15,21 +16,23 @@ class APIPolygonConnector(APIBaseConnector):
             ticker = self._get_cache('cusip_to_ticker', cusip)
             if ticker is not None: return ticker
 
-            # send request 
-            response = requests.get(
-                url=self.api_domain + 'tickers', 
-                params={
+            # send requests
+            attempts_count = 0
+            while True:
+                json_response = self._query_tickers_endpoint({
                     'apiKey': self.api_key,
                     'cusip': cusip,
                     'limit': 2
-                }
-            )
+                })
 
-            # verify response
-            response.raise_for_status()
-            json_response = response.json()
-            assert(json_response['status'] == 'OK')
-
+                if json_response is None and attempts_count > 3: 
+                    raise Exception('query attempts failed')
+                elif json_response is None: 
+                    attempts_count += 1
+                    time.sleep(1)
+                else:
+                    break
+    
             # extract ticker
             if json_response['results'] is None or len(json_response['results']) != 1: ticker = None
             else: ticker = str(json_response['results'][0]['ticker'])
@@ -44,4 +47,18 @@ class APIPolygonConnector(APIBaseConnector):
             self.logger.exception('Error in query_ticker_with_cusip: ' + str(e))
             return None
 
+    def _query_tickers_endpoint(self, params: dict) -> dict: 
+        try:
+            response = requests.get(
+                url=self.api_domain + 'tickers', 
+                params=params
+            )
 
+            # verify response
+            response.raise_for_status()
+            json_response = response.json()
+            assert(json_response['status'] == 'OK')
+            return json_response
+
+        except Exception:
+            return None
